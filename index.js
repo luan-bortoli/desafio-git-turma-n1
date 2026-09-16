8fb094da54050eba50e289fd4056537e6f3fa9b0
JOSÉ ANÍSIO PALHA BARBOSA JUNIOR <jose_barbosa-juni@estudante.sesisenai.org.br>
2026-09-16 20:56:49 -0300

    implementa função de divisão

--- index.js
+++ index.js
@@ -4,4 +4,12 @@ function somar(a, b) {
     return a + b;
 }
 
+function divisao(a, b) {
+   if (b === 0) {
+    return "Não é possível dividir por zero";
+   } 
+   return a / b;
+}
+
 console.log("10 + 5 =", somar(10, 5));
+console.log(divisao(10,2));
