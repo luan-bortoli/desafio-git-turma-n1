1f14140d8d9c5327b36cc102e59a790fa66f887e
EDUARDO RILOZI <eduardo_rilozi@estudante.sesisenai.org.br>
2026-09-16 20:54:10 -0300

    foi implementado um código para  exibir o maior número

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ function somar(a, b) {
     return a + b;
 }
 
+function maiorNumero (a, b) {
+    if (a>b){
+        return a;
+
+    }else{
+        return b;
+    }
+}
+
 console.log("10 + 5 =", somar(10, 5));
+console.log(maiorNumero(1, 10));
