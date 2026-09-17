0cc6c47edcb0ab0a6b00380b4edf00f5890a37b6
nicolas_t_souza <nicolas_t_souza@estudante.sesisenai.org.br>
2026-09-16 21:10:53 -0300

    foi feito um sistema para multiplicar dois numeros

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ function somar(a, b) {
     return a + b;
 }
 
+function multiplicar(numero, numero2){
+    return numero * numero2 ;
+}
+
 console.log("10 + 5 =", somar(10, 5));
+let numero = Number(prompt("digite um numero"))
+let numero2 = Number(prompt("digite um numero"))
+console.log(multiplicar(numero, numero2));
+
+
+ 
